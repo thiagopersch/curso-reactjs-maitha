f0ac90c3abd72f64a0e9a3bbbd5574ee0c18b0dc
Tiago Persch <thiagopersch@gmail.com>
2022-01-18 21:57:36 -0400

    Add js

---
+++ js/primitiveData.js
@@ -0,0 +1,23 @@
+/*
+
+******** Tipos de dados primitivos ********
+
+1. String: Tipo texto;
+2. Number: Tipo número;
+3. Undefined -> Ele não é alocado em nenhum lugar na memória;
+4. Null -> Ele não é alocado em nenhum lugar na memória;
+5. Boolean -> Só pode receber valores verdadeiros ou falso;
+
+*/
+
+const nome = "João";
+const nome1 = `João`;
+
+const num = 10;
+const num1 = 10.4;
+
+let nomeUndefined;
+let nomeNulo = null;
+
+const vivo = true;
+const morto = false;
