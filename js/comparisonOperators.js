11f8e445c4c00c265b69b6087f57241e1afac675
Tiago Persch <thiagopersch@gmail.com>
2022-01-23 18:00:13 -0400

    Estudando operadores de comparaçao

---
+++ js/comparisonOperators.js
@@ -0,0 +1,62 @@
+/**
+ * Operadores de comparação
+ *
+ * Sinal de maior(>)
+ * Sinal de maior-igual(>=)
+ * Sinal de menor (<)
+ * Sinal de menor-igual (<=)
+ * Sinal de igualdade(==)
+ * Sinal de igualdade-estrita(===)
+ * Sinal de diferença (!=)
+ * Sinal de diferença-estrita (!==)
+ */
+
+//Sinal de maior(>)
+const value1 = 1;
+const value2 = 2;
+
+const result = value1 > value2;
+
+console.log(result);
+
+//Sinal de maior-igual(>=)
+const value1 = 1;
+const value2 = 2;
+const result = value1 >= value2;
+console.log(result);
+
+//Sinal de menor (<)
+const value1 = 1;
+const value2 = 2;
+const result = value1 < value2;
+console.log(result);
+
+//Sinal de menor-igual (<=)
+const value1 = 1;
+const value2 = 2;
+const result = value1 <= value2;
+console.log(result);
+
+//Sinal de igualdade(==)
+const value1 = 2;
+const value2 = 2;
+const result = value1 == value2;
+console.log(result);
+
+//Sinal de igualdade-estrita(===)
+const value1 = 2;
+const value2 = "2";
+const result = value1 === value2;
+console.log(result);
+
+//Sinal de diferença (!=)
+const value1 = 2;
+const value2 = "2";
+const result = value1 != value2;
+console.log(result);
+
+//Sinal de diferença-estrita (!==)
+const value1 = 2;
+const value2 = "2";
+const result = value1 !== value2;
+console.log(result);
