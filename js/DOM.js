c4e1b44b085c02c8bf68b183a1a05f91ac45ee77
Tiago Persch <thiagopersch@gmail.com>
2022-01-27 20:18:39 -0400

    DOM

---
+++ js/DOM.js
@@ -0,0 +1,8 @@
+/**
+ * O que é o DOM(Document Object Model)?
+ *
+ * É os elementos correspondentes as árvores de elementos do browser
+ */
+
+//querySelector: informar a propriedade do elemento que deseja.
+document.querySelector();
--- js/String.js
+++ js/String.js
@@ -7,34 +7,45 @@ let umaString = "Uma String";
 console.log(umaString[2]);
 
 // função nativa do Javascript para pegar o caracter da string
+let umaString = "Uma String";
 console.log(umaString.charAt(0));
 
 // Função nativa do Javascript para pegar aonde começa uma string (Esquerda para direita)
+let umaString = "Uma String";
 console.log(umaString.indexOf("String"));
 
 // Função nativa do Javascript para pegar aonde começa uma string (Direita para direita)
+let umaString = "Uma String";
 console.log(umaString.lastIndexOf("String"));
 
 // Função nativa do javascript para pegar as palavras de acordo com o pattern(regex)
+let umaString = "Uma String";
 console.log(umaString.match(/[a-z]/g));
 
 // Função nativa do javascript para pegar o index da palavra. Muito parecido com o indexOf, porém o search aceita expressão regular(regex)
+let umaString = "Uma String";
 console.log(umaString.search(/i/));
 
 // Função nativa do javascript para substituir digitos. Você pode escolher entre querer usar ou não expressão regular(regex)
+let umaString = "Uma String";
 console.log(umaString.replace("Uma", "Outra"));
 
 // Função nativa do javascript para retornar o numero de strings
+let umaString = "Uma String";
 console.log(umaString.length);
 
 // Função nativa do javascript usada para recortar palavras, obs: para retornar a palavra inteira, deve colocar o valor do index um digito a frente da palavra requerida
+let umaString = "Uma String";
 console.log(umaString.slice(0, 3));
 
 // Função nativa do javascript usada para dividir string
+let umaString = "Uma String";
 console.log(umaString.split);
 
 // Função nativa do javascript usada para colocar sua string em caixa alta
+let umaString = "Uma String";
 console.log(umaString.toUpperCase());
 
 // Função nativa do javascript usada para colocar sua string em caixa baixa
+let umaString = "Uma String";
 console.log(umaString.toLowerCase());
